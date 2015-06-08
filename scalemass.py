3da787d75760d88f65617f1430a7ac391969c394
asvath <ashaasvathaman@gmail.com>
2015-06-08 12:54:08 -0700

    edited

--- scalemass.py
+++ scalemass.py
@@ -29,9 +29,15 @@ def readconvert(filename,startnum,stepsize,filenum):
 	vz=vz[startnum::stepsize]
 	print len(x)
 #	m=np.concatenate((m[:len(m)/100]*0.53/0.9,m[len(m)/100:]))
-        m=np.concatenate((m[:len(m)/100:2]*0.53/0.9,m[1:len(m)/100:2],m[len(m)/100:]))
+    m=np.concatenate((m[:len(m)/100:2]*0.53/0.9,m[1:len(m)/100:2],m[len(m)/100:]))
 	m=(mnew/np.sum(m))*m
 
+    x=np.concatenate((x[:len(x)/100:2],x[1:len(x)/100:2],x[len(x)/100:]))
+    y=np.concatenate((y[:len(y)/100:2],y[1:len(y)/100:2],y[len(y)/100:]))
+    z=np.concatenate((z[:len(z)/100:2],z[1:len(z)/100:2],z[len(z)/100:]))
+    vx=np.concatenate((vx[:len(vx)/100:2],vx[1:len(vx)/100:2],vx[len(vx)/100:]))
+    vy=np.concatenate((vy[:len(vy)/100:2],vy[1:len(vy)/100:2],vy[len(vy)/100:]))
+    vz=np.concatenate((vz[:len(vz)/100:2],vz[1:len(vz)/100:2],vz[len(vz)/100:]))
         
 
 	np.transpose(np.concatenate(([x,y,z,m],[vx,vy,vz,m]),axis=1)).astype('float32').tofile("asha.NBODY_%03d" %filenum)
